63f34ba1857bdbc8bd9692e9fcf816df9910c1fc
Hazel Antonio Santos Quiroz <143685351+DoomK1ll3r@users.noreply.github.com>
2024-05-10 13:17:45 -0600

    Add functionality to number buttons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const input = document.getElementById("input-area");
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+const btn4 = document.getElementById("btn-4");
+const btn5 = document.getElementById("btn-5");
+const btn6 = document.getElementById("btn-6");
+const btn7 = document.getElementById("btn-7");
+const btn8 = document.getElementById("btn-8");
+const btn9 = document.getElementById("btn-9");
+const btn0 = document.getElementById("btn-0");
+
 let firstOperand = 0;
 let secondOperand = 0;
 let operator = 0;
@@ -37,3 +49,18 @@ function operate (operator, firstOperand = 0, secondOperand = 0) {
       break;
   }
 }
+
+function updateDisplay (digit) {
+  input.innerText += `${digit}`;
+}
+
+btn1.addEventListener("click", () => updateDisplay('1'));
+btn2.addEventListener("click", () => updateDisplay('2'));
+btn3.addEventListener("click", () => updateDisplay('3'));
+btn4.addEventListener("click", () => updateDisplay('4'));
+btn5.addEventListener("click", () => updateDisplay('5'));
+btn6.addEventListener("click", () => updateDisplay('6'));
+btn7.addEventListener("click", () => updateDisplay('7'));
+btn8.addEventListener("click", () => updateDisplay('8'));
+btn9.addEventListener("click", () => updateDisplay('9'));
+btn0.addEventListener("click", () => updateDisplay('0'));
